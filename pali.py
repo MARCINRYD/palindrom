28c33e35b991b1a64a0b3f6fc002e9d6b5f268e6
Marcin Rydalski <marcin.rydalski1@gmail.com>
2025-06-02 19:46:26 +0200

    Dodano funkcję sprawdzającą palindromy

--- pali.py
+++ pali.py
@@ -1,3 +1,8 @@
 
 def czy_palindrom(tekst: str) -> bool:
     return tekst == tekst[::-1]
+print(czy_palindrom("kajak"))  # True
+print(czy_palindrom("potop"))  # True   
+print(czy_palindrom("python"))  # False
+print(czy_palindrom("level"))  # True   
+print(czy_palindrom("gucz"))
